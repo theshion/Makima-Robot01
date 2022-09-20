b3f416e5a8dec5794cde314e7f2acc5f3530b837
OTAKU • BINGE <113184521+otakubinge@users.noreply.github.com>
2022-09-20 12:21:40 +0530

    Update connection.py

--- MakimaRobot/modules/connection.py
+++ MakimaRobot/modules/connection.py
@@ -1,3 +1,8 @@
+# ɪғ ʏᴏᴜ ғᴏᴜɴᴅ ɪɴ ᴀɴʏ ᴇʀʀᴏʀs ᴛʜᴀɴ ᴘʟᴢ ᴄᴏɴᴛᴀᴄᴛ @SIXTH_H0KAGE
+# sᴜᴘᴘᴏʀᴛ :- @kakashi_bots_support
+# ᴜᴘᴅᴀᴛᴇs :- @kakashi_bots_updates
+# ɴᴇᴛᴡᴏʀᴋ :- @Otaku_Binge
+
 import re
 import time
 
@@ -386,19 +391,19 @@ def connect_button(update, context):
         connect_chat(update, context)
 
 
-__mod_name__ = "Cᴏɴɴᴇᴄᴛ"
+__mod_name__ = "Connect🔗"
 
 __help__ = """
 Sometimes, you just want to add some notes and filters to a group chat, but you don't want everyone to see; This is where connections come in...
 This allows you to connect to a chat's database, and add things to it without the commands appearing in chat! For obvious reasons, you need to be an admin to add things; but any member in the group can view your data.
 
- ❍ /connect: Connects to chat (Can be done in a group by /connect or /connect <chat id> in PM)
- ❍ /connection: List connected chats
- ❍ /disconnect: Disconnect from a chat
- ❍ /helpconnect: List available commands that can be used remotely
+ ❂ /connect: Connects to chat (Can be done in a group by /connect or /connect <chat id> in PM)
+ ❂ /connection: List connected chats
+ ❂ /disconnect: Disconnect from a chat
+ ❂ /helpconnect: List available commands that can be used remotely
 
 *Admin only:*
- ❍ /allowconnect <yes/no>: allow a user to connect to a chat
+ ❂ /allowconnect <yes/no>: allow a user to connect to a chat
 """
 
 CONNECT_CHAT_HANDLER = CommandHandler("connect", connect_chat, pass_args=True)
