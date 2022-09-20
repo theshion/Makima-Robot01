8ef0ccc1c349dfcdf2cf2e370eaa15b32c2ec868
OTAKU • BINGE <113184521+otakubinge@users.noreply.github.com>
2022-09-20 13:00:05 +0530

    Update global_bans.py

--- MakimaRobot/modules/global_bans.py
+++ MakimaRobot/modules/global_bans.py
@@ -531,7 +531,7 @@ dispatcher.add_handler(UNGBAN_HANDLER)
 dispatcher.add_handler(GBAN_LIST)
 dispatcher.add_handler(GBAN_STATUS)
 
-__mod_name__ = "Aɴᴛɪ-Sᴘᴀᴍ​"
+__mod_name__ = "Anti-Spam​👹"
 __handlers__ = [GBAN_HANDLER, UNGBAN_HANDLER, GBAN_LIST, GBAN_STATUS]
 
 if STRICT_GBAN:  # enforce GBANS if this is set
