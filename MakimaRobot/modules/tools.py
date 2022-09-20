413d7f53469fed7163a9696f67b4b70ead0491fb
OTAKU • BINGE <113184521+otakubinge@users.noreply.github.com>
2022-09-20 13:27:32 +0530

    Update tools.py

--- MakimaRobot/modules/tools.py
+++ MakimaRobot/modules/tools.py
@@ -2,11 +2,12 @@
 # sᴜᴘᴘᴏʀᴛ :- @kakashi_bots_support
 # ᴜᴘᴅᴀᴛᴇs :- @kakashi_bots_updates
 # ɴᴇᴛᴡᴏʀᴋ :- @Otaku_Binge
-__mod_name__ = "Tᴏᴏʟs"
+
+__mod_name__ = "Tᴏᴏʟs⚒️"
 
 __help__ = """
 
 *Converts*
- ❍ /encrypt*:* Encrypts The Given Text
- ❍ /decrypt*:* Decrypts Previously Ecrypted Text
+ ❂ /encrypt*:* Encrypts The Given Text
+ ❂ /decrypt*:* Decrypts Previously Ecrypted Text
 """
