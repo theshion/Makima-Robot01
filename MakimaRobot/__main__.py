e26ce77469f2b1264ad4dbd360a19025edcd53c9
OTAKU • BINGE <113184521+otakubinge@users.noreply.github.com>
2022-09-20 10:35:12 +0530

    Update __main__.py

--- MakimaRobot/__main__.py
+++ MakimaRobot/__main__.py
@@ -717,23 +717,14 @@ def main():
     help_callback_handler = CallbackQueryHandler(help_button, pattern=r"help_.*")
 
     settings_handler = CommandHandler("settings", get_settings)
-    settings_callback_handler = CallbackQueryHandler(settings_button, pattern=r"stngs_")
-
-    about_callback_handler = CallbackQueryHandler(
-        Fallen_about_callback, pattern=r"fallen_"
-    )
-    source_callback_handler = CallbackQueryHandler(
-        Source_about_callback, pattern=r"source_"
-    )
-
+    settings_callback_handler = CallbackQueryHandler(settings_button, pattern=r"stngs_")    
+    
     donate_handler = CommandHandler("donate", donate)
     migrate_handler = MessageHandler(Filters.status_update.migrate, migrate_chats)
 
     dispatcher.add_handler(test_handler)
     dispatcher.add_handler(start_handler)
-    dispatcher.add_handler(help_handler)
-    dispatcher.add_handler(about_callback_handler)
-    dispatcher.add_handler(source_callback_handler)
+    dispatcher.add_handler(help_handler)        
     dispatcher.add_handler(settings_handler)
     dispatcher.add_handler(help_callback_handler)
     dispatcher.add_handler(settings_callback_handler)
