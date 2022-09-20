a6590a8a8d07e9494ca93fdc32571f61d7d8da42
OTAKU • BINGE <113184521+otakubinge@users.noreply.github.com>
2022-09-20 13:27:02 +0530

    Update tiny.py

--- MakimaRobot/modules/tiny.py
+++ MakimaRobot/modules/tiny.py
@@ -84,7 +84,7 @@ async def _(event):
     os.remove(ik)
 
 
-__mod_name__ = "Tɪɴʏ"
+__mod_name__ = "Tɪɴʏ▪️"
 __help__ = """
-❍ /tiny*:* reply a sticker and see magic
+❂ /tiny*:* reply a sticker and see magic
 """
