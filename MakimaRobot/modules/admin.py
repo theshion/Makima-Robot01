ef4dfefe76012cf6b910411f26d2ed6e747473ce
OTAKU • BINGE <113184521+otakubinge@users.noreply.github.com>
2022-09-20 11:58:14 +0530

    Update admin.py

--- MakimaRobot/modules/admin.py
+++ MakimaRobot/modules/admin.py
@@ -1035,7 +1035,7 @@ dispatcher.add_handler(DEMOTE_HANDLER)
 dispatcher.add_handler(SET_TITLE_HANDLER)
 dispatcher.add_handler(ADMIN_REFRESH_HANDLER)
 
-__mod_name__ = "Aᴅᴍɪɴs"
+__mod_name__ = "Admins"
 __command_list__ = [
     "setdesc" "setsticker" "setgpic" "delgpic" "setgtitle" "adminlist",
     "admins",
