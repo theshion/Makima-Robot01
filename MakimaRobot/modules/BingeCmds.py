552a9024e0150adfd639dc1676ec7d4e9c6bf07b
OTAKU • BINGE <113184521+otakubinge@users.noreply.github.com>
2022-09-20 13:48:02 +0530

    Create BingeCmds.py

---
+++ MakimaRobot/modules/BingeCmds.py
@@ -0,0 +1,36 @@
+__mod_name__ = "MORE🔥"
+
+__help__ = """
+❂ /animec - our anime channel.
+❂ /otaku - our network.
+❂ /sex - sex dedo.
+❂ /tts lang | text - text to speech.
+❂ /about , /repo , /source - makima history,repo etc.
+
+*ANIMATION*
+❂ /kill
+❂ /bombs
+❂ /hack
+❂ /moon
+❂ /clock
+❂ /block
+❂ /earth
+
+*BLACKLISTS USERS*
+❂ /ignore 
+❂ /notice
+❂ /ignorelist
+
+*BUG*
+❂ /bug - report a but to support chat
+
+❂ /carbon - make carbon if replied to a text
+
+❂ /cash 
+❂ /myinfo
+❂ /ping
+❂ /react
+❂ /wallpaper 
+❂ /vsong or /video
+❂ /watchorder <anime name>
+❂ /whois
