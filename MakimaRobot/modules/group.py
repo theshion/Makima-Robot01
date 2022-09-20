b817dc6ac42a3bb1bb106c969939ebc8ee101a7f
OTAKU • BINGE <113184521+otakubinge@users.noreply.github.com>
2022-09-20 13:01:42 +0530

    Update group.py

--- MakimaRobot/modules/group.py
+++ MakimaRobot/modules/group.py
@@ -1,9 +1,9 @@
 __help__ = """
- ❍ /setgtitle <newtitle>*:* Sets new chat title in your group.
- ❍ /setgpic*:* As a reply to file or photo to set group profile pic!
- ❍ /delgpic*:* Same as above but to remove group profile pic.
- ❍ /setsticker*:* As a reply to some sticker to set it as group sticker set!
- ❍ /setdescription <description>*:* Sets new chat description in group.
+ ❂ /setgtitle <newtitle>*:* Sets new chat title in your group.
+ ❂ /setgpic*:* As a reply to file or photo to set group profile pic!
+ ❂ /delgpic*:* Same as above but to remove group profile pic.
+ ❂ /setsticker*:* As a reply to some sticker to set it as group sticker set!
+ ❂ /setdescription <description>*:* Sets new chat description in group.
 """
 
-__mod_name__ = "Gʀᴏᴜᴘ"
+__mod_name__ = "Group🌟"
