991a72f4d54a15531f3bbf96bb44af2ffa0f1d26
OTAKU • BINGE <113184521+otakubinge@users.noreply.github.com>
2022-09-20 13:30:31 +0530

    Update userinfo.py

--- MakimaRobot/modules/userinfo.py
+++ MakimaRobot/modules/userinfo.py
@@ -514,26 +514,26 @@ def __user_info__(user_id):
 
 __help__ = """
 *ID:*
- ❍ /id*:* get the current group id. If used by replying to a message, gets that user's id.
- ❍ /gifid*:* reply to a gif to me to tell you its file ID.
+ ❂ /id*:* get the current group id. If used by replying to a message, gets that user's id.
+ ❂ /gifid*:* reply to a gif to me to tell you its file ID.
 
 *Self added information:* 
- ❍ /setme <text>*:* will set your info
- ❍ /me*:* will get your or another user's info.
+ ❂ /setme <text>*:* will set your info
+ ❂ /me*:* will get your or another user's info.
 *Examples:* 💡
  ➩ /setme I am a wolf.
  ➩ /me @username(defaults to yours if no user specified)
 
 *Information others add on you:* 
- ❍ /bio*:* will get your or another user's bio. This cannot be set by yourself.
- ❍ /setbio <text>*:* while replying, will save another user's bio 
+ ❂ /bio*:* will get your or another user's bio. This cannot be set by yourself.
+ ❂ /setbio <text>*:* while replying, will save another user's bio 
 *Examples:* 💡
  ➩ /bio @username(defaults to yours if not specified).`
  ➩ /setbio This user is a wolf` (reply to the user)
 
 *Overall Information about you:*
- ❍ /info*:* get information about a user. 
- ❍ /myinfo*:* Shows info about the user who sent this command.
+ ❂ /info*:* get information about a user. 
+ ❂ /myinfo*:* Shows info about the user who sent this command.
 """
 
 SET_BIO_HANDLER = DisableAbleCommandHandler("setbio", set_about_bio)
@@ -556,7 +556,7 @@ dispatcher.add_handler(GET_BIO_HANDLER)
 dispatcher.add_handler(SET_ABOUT_HANDLER)
 dispatcher.add_handler(GET_ABOUT_HANDLER)
 
-__mod_name__ = "Iɴꜰᴏs"
+__mod_name__ = "Iɴꜰᴏs📚"
 __command_list__ = ["setbio", "bio", "setme", "me", "info"]
 __handlers__ = [
     ID_HANDLER,
