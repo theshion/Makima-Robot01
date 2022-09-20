78b023965f258ee10fb938aa43746f84d3f750c0
OTAKU • BINGE <113184521+otakubinge@users.noreply.github.com>
2022-09-20 13:37:32 +0530

    Update zip.py

--- MakimaRobot/modules/zip.py
+++ MakimaRobot/modules/zip.py
@@ -216,8 +216,8 @@ def get_lst_of_files(input_directory, output_lst):
 
 __help__ = """
 Hey I can convert files here..
- ❍ /zip*:* reply to a telegram file to compress it in .zip format
- ❍ /unzip*:* reply to a telegram file to decompress it from the .zip format
+ ❂ /zip*:* reply to a telegram file to compress it in .zip format
+ ❂ /unzip*:* reply to a telegram file to decompress it from the .zip format
 """
 
 __mod_name__ = "Zipping🤐​"
